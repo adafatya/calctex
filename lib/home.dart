1fc796269c211a2599799349cc9a6a14c8861433
adafatya <aufaditya1895@gmail.com>
2021-04-23 04:44:46 +0700

    Mengubah sedikit tatanan home screen

--- lib/home.dart
+++ lib/home.dart
@@ -7,32 +7,46 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text("Beta Calculator"),
       ),
-      body: ListView(
-        children: [
-          ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/materi');
-              },
-              child: Column(
-                children: [
-                  Icon(Icons.book),
-                  Text("Materi")
-                ],
-              )
-          ),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/kalku');
-              },
-              child: Column(
-                children: [
-                  Icon(Icons.calculate),
-                  Text("Kalkulator")
-                ]
-              )
-          )
-        ]
-      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(bottom: 150)),
+            Container(
+              height: 100,
+              width: 100,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/materi');
+                },
+                child: Column(
+                  children: [
+                    Padding(padding: EdgeInsets.only(bottom: 10)),
+                    Icon(Icons.book, size: 50),
+                    Text("Materi")
+                  ],
+                )
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(bottom: 50)),
+            Container(
+              height: 100,
+              width: 100,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/kalku');
+                },
+                child: Column(
+                  children: [
+                    Padding(padding: EdgeInsets.only(bottom: 10)),
+                    Icon(Icons.calculate, size: 50),
+                    Text("Kalkulator")
+                  ],
+                )
+              ),
+            ),
+          ]
+        ),
+      )
     );
   }
 }
