ec9dd87513afd6c39d4202d78889da3517fde5cf
adafatya <aufaditya1895@gmail.com>
2021-04-23 05:04:51 +0700

    add color to list tile

--- lib/materi.dart
+++ lib/materi.dart
@@ -9,11 +9,23 @@ class Materi extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          ListTile(
-            title: Text("Persamaan Kuadrat"),
-            onTap: (){
-              Navigator.pushNamed(context, "/qe");
-            },
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(width: 2, color: Colors.blue),
+              boxShadow: [BoxShadow(color: Colors.blue[400])]
+            ),
+            child: ListTile(
+              title: Text("Persamaan Kuadrat",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "Sans serif",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20)
+                      ),
+              onTap: (){
+                Navigator.pushNamed(context, "/qe");
+              },
+            ),
           )
         ],
       )
